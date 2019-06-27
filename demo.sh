10e6630f3aaf2d127078865001d817185bd23073
Wolfgang Richter <wolf@soroco.com>
2019-06-27 11:21:50 -0400

    pyce: copyright header update to the year

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#   Copyright 2017-18 Soroco Americas Private Limited
+#   Copyright 2017-19 Soroco Americas Private Limited
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
--- pyce/__init__.py
+++ pyce/__init__.py
@@ -1,4 +1,4 @@
-#   Copyright 2016-18 Soroco Americas Private Limited
+#   Copyright 2016-19 Soroco Americas Private Limited
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
